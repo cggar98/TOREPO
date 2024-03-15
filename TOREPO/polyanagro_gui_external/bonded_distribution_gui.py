0ef4663358777fd0b30309db79646555e25db460
cggar98 <carlos.garcia@iem.cfmac.csic.es>
2024-03-15 10:14:05 +0100

    Add files via upload

---
+++ TOREPO/polyanagro_gui_external/bonded_distribution_gui.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+def func_page_bonded_distribution():
+
+
+    st.markdown("<h1 style='font-size:24px;'>Bonded Distribution</h1>", unsafe_allow_html=True)
+
+
+
+
+def run_page_bonded_distribution():
+
+    func_page_bonded_distribution()
---
+++ TOREPO/polyanagro_gui_external/neighbor_sphere_gui.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+def func_page_neighbor_sphere():
+
+
+    st.markdown("<h1 style='font-size:24px;'>Neighbor Sphere</h1>", unsafe_allow_html=True)
+
+
+
+
+def run_page_neighbor_sphere():
+
+    func_page_neighbor_sphere()
